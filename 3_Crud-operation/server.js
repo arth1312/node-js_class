bc3d25f972f4052cd112933f12e2563e875e03a4
arth1312 <arthkoradiya@gmail.com>
2025-09-12 12:56:00 +0530

    3_Crud-Operation 12/09/2025

---
+++ 3_Crud-operation/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const server = express();
+
+let persons = [
+    {
+        id: 101,
+        name: "Arth Koradiya",
+        email: "arth@test.in",
+        job: "full-stack development"
+    },
+    {
+        id: 102,
+        name: "Parth Patel",
+        email: "parth@test.in",
+        job: "game development"
+    },
+    {
+        id: 103,
+        name: "Sumedh Mudgalkar",
+        email: "sumedh@test.in",
+        job: "Actor"
+    }
+];
+
+server.use(express.urlencoded());
+
+server.set("view engine", "ejs");
+
+server.get('/', (req, res) => {
+    res.render('index', { persons });
+});
+
+server.get('/add_person', (req, res) => {
+    res.render('addPerson');
+});
+
+server.post('/add_person', (req, res) => {
+    const newPerson = req.body;
+    persons.push(newPerson);
+    res.redirect('/');
+});
+
+let port = 8001;
+
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});

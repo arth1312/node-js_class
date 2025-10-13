c494d751877159d0e60dc976a45c29cddd1026ec
arth1312 <arthkoradiya@gmail.com>
2025-10-13 17:42:05 +0530

    8_Admin_panel_setup 13/10/2025

--- 8_Admin_panel_setup/routes/index.js
+++ 8_Admin_panel_setup/routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { dashboard, loginpage,Blogs, loginUser, logOut, profile, passwordChangeForm, passwordChange, myBlog, sendOtp, OTPForm, newPassword, verifyOTP, resetPassword,  } = require("../controllers/index");
+const { dashboard, loginpage, loginUser, logOut, profile, passwordChangeForm, passwordChange } = require("../controllers/index");
 const userRrouter = require("./user.route");
 const blogRrouter = require("./blog.route");
 const router = express.Router();
@@ -9,17 +9,8 @@ router.get("/", loginpage);
 router.post("/login-user", loginUser);
 router.get("/user-logOut", logOut);
 router.get("/user-profile", profile);
-router.get("/user-passwordChange",  passwordChangeForm);
+router.get("/user-passwordChange", passwordChangeForm);
 router.post("/passwordChange", passwordChange);
-router.get("/my-blog/category/:category",myBlog);
-// router.get("/my-blog", myBlog);
-
-router.post("/sendOtp",sendOtp);
-router.get("/OTPForm",OTPForm);
-router.get("/resetPasswordForm",newPassword);
-router.post("/verifyOTP",verifyOTP);
-router.post("/resetPassword", resetPassword)
-
 
 router.use("/users", userRrouter);
 router.use("/blogs", blogRrouter);

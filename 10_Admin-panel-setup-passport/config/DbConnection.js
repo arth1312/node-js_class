fbdd6cf9e38b9072c4c4f69143d9bd9064c25558
arth1312 <arthkoradiya@gmail.com>
2025-11-11 13:28:46 +0530

    10_Admin-panel-setup-passport 11/11/2025

--- 10_Admin-panel-setup-passport/config/DbConnection.js
+++ 10_Admin-panel-setup-passport/config/DbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect("mongodb+srv://arth1312:Arth1312@cluster0.t7hd3z1.mongodb.net/admin-panel");
+    await mongoose.connect("mongodb+srv://arth1312:Arth1312@cluster0.t7hd3z1.mongodb.net/adminpanel");
     // await mongoose.connect("");
     console.log("Database Connected Successfully");
   } catch (err) {
